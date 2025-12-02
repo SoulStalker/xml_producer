bd4e4147479fe6abbc8a23531bf7128cbd211926
Almaz Galimov <almazwork@gmail.com>
2025-12-02 15:56:32 +0500

    add load config

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/SoulStalker/xml_producer/internal/config"
+)
+
 func main() {
 	// init config
-
+	cfg := config.MustLoad()
+	fmt.Println(cfg.Kafka.Brokers[0])
+	fmt.Println(cfg.Storage.GetRetentionDuration())
 
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,11 +3,42 @@ package config
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
+	Kafka   KafkaConfig   `yaml:"kafka"`
+	Storage StorageConfig `yaml:"storage"`
+	App     AppConfig     `yaml:"app"`
+
+	// Redis настройки
+	RedisHost     string `yaml:"redis_host" env:"REDIS_HOST" env-default:"localhost"`
+	RedisPort     string `yaml:"redis_port" env:"REDIS_PORT" env-default:"6379"`
+	RedisPassword string `yaml:"redis_password" env:"REDIS_PASSWORD" env-default:""`
+	RedisDB       int    `yaml:"redis_db" env:"REDIS_DB" env-default:"0"`
+}
+
+type KafkaConfig struct {
+	Brokers        []string `yaml:"brokers" env-required:"true"`
+	Topic          string   `yaml:"topic" env-required:"true"`
+	DLQTopic       string   `yaml:"dlq_topic"`
+	MaxRetries     int      `yaml:"max_retries" env-default:"3"`
+	RetryBackoffMs int      `yaml:"retry_backoff_ms" env-default:"1000"`
+	BatchTimeoutMs int      `yaml:"batch_timeout_ms" env-default:"100"`
+	Compression    string   `yaml:"compression" env-default:"gzip"`
+}
+
+type StorageConfig struct {
+	NFSPath             string `yaml:"nfs_path"`
+	BackupPath          string `yaml:"backup_path"`
+	BackupRetentionDays int    `yaml:"backup_retention_days" env-default:"30"`
+}
+
+type AppConfig struct {
+	PollIntervalSec int    `yaml:"poll_interval_sec" env-default:"10"`
+	LogLevel        string `yaml:"log_level" env-default:"info"`
 }
 
 func MustLoad() *Config {
@@ -22,4 +53,8 @@ func MustLoad() *Config {
 	}
 
 	return &cfg
+}
+
+func (c *StorageConfig) GetRetentionDuration() time.Duration {
+	return time.Duration(c.BackupRetentionDays) * 24 * time.Hour
 }
